4f38c4956f938bfa2f2dc5ffe3911bcf5bbdb7e8
amine <amineotako64@gmail.com>
2024-11-15 11:30:18 +0100

    i added something new

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    char phonenumbr[50];
+    char name[50];
+    printf("enter the contact name:\n");
+    scanf("%s",&name);
+    printf("enter the contact number:\n");
+    scanf("%s",&phonenumbr);
+    printf("the contact is ")
+
+    return 0;
+}
